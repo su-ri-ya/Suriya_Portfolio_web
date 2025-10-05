e8ca4242aadaf4c596dcf268f6ab632dff096433
Suriya <suriya6374570440@gmail.com>
2025-10-05 21:11:28 +0530

    env change

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-mongoose.connect(process.env.MONGO_URI, {})
+const MONGO_URI="mongodb+srv://suriya:knA2fPcmPEQnfVgk@cluster0.xot2c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.connect(MONGO_URI, {})
     .then(() => console.log('Connected to MongoDB Atlas'))
     .catch((err) => console.error('Error connecting to MongoDB:', err));
 
@@ -38,7 +39,7 @@ app.use("/api/", ResumeRoutes);
 app.use("/api/", certificateRoutes);
 
 // Set the port from environment variables or default to 5000
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7777;
 
 // Start the server
 app.listen(PORT, () => {
